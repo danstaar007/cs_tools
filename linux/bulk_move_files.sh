9019b3d6f4bec79d9fc0150ba39ea9d0ebbf3ff0
Guy Cooper <guycooper@danstaar.com>
2024-07-03 16:18:31 -0700

    added multi threading

--- linux/bulk_move_files.sh
+++ linux/bulk_move_files.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 # array for each folder that needs to be moved and archived
-# It works by adding (top level folder,1st find mindepth, 1st find maxdepth, 2nd find mindepth, 2nd find maxdepth)
+# It works by adding (top level folder, mtime for days/ mmin for mins, 1st find mindepth, 1st find maxdepth, 2nd find mindepth, 2nd find maxdepth, destination folder)
 # the intent is the top level folder contains the week folder of the month and year (top level)
 # when archived, depending on the min/max depth, you will get different resulting naming conventions
 # try to get the folder of the week to be the archived folder
 
+# function to call after the find
+# may be the way I am multi threading??
+function tar_folder() {
+    folder=$1
+    tar --null -czf "$global_save_location/$archive" "$folder" && echo "$folder complete"
+}
+
+# array for each folder that needs to be moved and archived
+# It works by adding (top level folder, mtime for days/ mmin for mins, 1st find mindepth, 1st find maxdepth, 2nd find mindepth, 2nd find maxdepth, destination folder)
+# the intent is the top level folder contains the week folder of the month and year (top level)
+# when archived, depending on the min/max depth, you will get different resulting naming conventions
+# try to get the folder of the week to be the archived folder
 declare -A locations=(
-    ["location1"]="/linux/2024/04-01-20242024,0,1,1,1,60"
-    ["location2"]="/domain_folder/2024/04-01-2024,0,2,1,2,60"
-    ["location3"]="/domain_folder/2023/04-01-2023,0,2,1,2,60"
+    ["location1"]="/linux/2024/04-01-20242024,mtime,2,2,0,2,60,/destination/folder"
+   # ["location2"]="/linux/2024/04-01-20242024,mtime,2,2,0,2,60,/destination/folder"
+   # ["location3"]="/linux/2024/04-01-20242024,mtime,2,2,0,2,60,/destination/folder"
     # Add more locations as needed
 )
 
@@ -21,27 +33,39 @@ for key in "${!locations[@]}"; do
     first_maxdepth="${params[2]}"
     second_mindepth="${params[3]}"
     second_maxdepth="${params[4]}"
-    days_old="${params[5]}"
+    days_or_min="${params[5]}"
+    days_old="${params[6]}"
+    global_save_location=$"{key}"
 
     # start at base dir and find all files
     # the first two numbers in the array are used for this min/maxdepth (x,min, max, x, x,x)
-   find "$top_level" -mindepth "$first_mindepth" -maxdepth "$first_maxdepth" -type d | while read base_name; do
+   find "$top_level" -mindepth "$first_mindepth" -maxdepth "$first_maxdepth" -$days_or_min + $days_old -type d | while read base_name; do
         
         # Extract year and month for use when naming the TAR files
+        # $archive_name is the ["location_x"] from the array
         year=$(basename "$(dirname "$base_name")")
         month=$(basename "$base_name")
-        archive="${year}_${month}.tar.gz"
+        archive="$archive_name_${year}_${month}.tar.gz"
 
     # compress files older than 60 days and use the year and month to name the TAR files
-    # if you are trying to test this on a local folder, instead of -mtime, use -mmin for minutes.
+    # if you are trying to test this on a local folder, make sure you change -mtime to -mmin for minutes in the array above.
     # make sure when testing you setup the folder structure exactly like the hierarchy you are executing this on.
     
-    # the second two numbers in the array are used for this min/maxdepth (x,min, max, x, x,x)
-    find "$base_name" -type f -mtime +"$days_old" -mindepth "$second_mindepth" -maxdepth "$second_maxdepth" -print0 | tar --null --directory="$(dirname "$base_name")" -czvf "/working/test_move/$archive" --transform "s,^$month,$year/$month," --files-from -
-        
+    # this is the loop that create 6 jobs and waits until one becomes available to start the next
+    # it calls the tar_folder function from above
+    for folder in "$base_name"; do
+        while (( $(jobs|wc -l) >=6 )); do
+            sleep 1
+        done
+        echo "$folder archive started" 
+        tar_folder "$folder" &
+    done
+
+    # WARNING!!! THIS WILL REMOVE THE ORGINAL FILES FROM THE ORIGINAL FILE LOCATION
     # removes the old files/folders from original location
-    find "$base_name" -type f -mtime +"$days_old" -exec rm {} +
-    find "$base_name" -mindepth 1 -type d -empty -delete
+    # uncomment the finds for this to work
+    #find "$base_name" -type f -mtime +"$days_old" -exec rm {} +
+    #find "$base_name" -mindepth 1 -type d -empty -delete
 
     done
 done
